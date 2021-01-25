bd870b2b615f7756f8fb7e0c4ca2da01b3fe073f
zobayerhasan99 <zobayer.hp5@gmail.com>
2021-01-25 23:23:03 +0600

    master code succesfully done

---
+++ Java-1st-Project/javanayem.java
@@ -0,0 +1,16 @@
+package javanayem;
+
+public class javanayem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hwllo world");
+		System.out.println("Nobody can hlps me ");
+		System.out.println("1 \t 2");
+		System.out.println("3 \t 4");
+		
+	
+	}
+
+}
