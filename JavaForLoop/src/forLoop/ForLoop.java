2cf2b8a38b4709a6b080f665eb91e53aabc55918
zobayerhasan99 <zobayer.hp5@gmail.com>
2021-03-20 11:06:11 +0600

    Master Code submit Done

---
+++ JavaForLoop/src/forLoop/ForLoop.java
@@ -0,0 +1,15 @@
+package forLoop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=1; i<=100; i=i+2)
+		{
+			System.out.println(i+ ".  I Love You Nayem");
+		}
+	}
+
+}
